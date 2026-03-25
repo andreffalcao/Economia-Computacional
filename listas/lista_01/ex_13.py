d49e6d0c63c20d6aeca9a27667a3104f916a1fdd
André Falcão <datafxlcon@gmail.com>
2026-03-25 15:12:02 -0300

    feat: adiciona o exercício 13

---
+++ listas/lista_01/ex_13.py
@@ -0,0 +1,10 @@
+# Estrutura de Controle Sequência - exercício 13
+# Entrada de Dados
+renda = float(input("Qual a renda do indivíduo? "))
+pbem = float(input("Qual o valor do bem? "))
+
+# Processamento de Dados
+fdem = renda / pbem
+
+# Saída de Dados
+print(f"Resposta 13: A quantidade demandada do bem é [{fdem:.2f}]")
