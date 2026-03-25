d23bb6c601924e945cd37de36d6b1e659921d32a
André Falcão <datafxlcon@gmail.com>
2026-03-25 14:33:27 -0300

    feat: adiciona exercício 02

---
+++ listas/lista_01/ex_02.py
@@ -0,0 +1,10 @@
+# Estrutura de Controle Sequência - exercício 02
+# Alocação de Memória
+num1 = 2 
+num2 = 3
+
+# Processamento de Dados
+soma = num1 + num2
+
+# Saída de Dados
+print(f"Resposta 2: {soma}")
