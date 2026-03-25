9020c86f055a3415a4b0517c6c1caa30bf44789d
André Falcão <datafxlcon@gmail.com>
2026-03-25 15:08:57 -0300

    feat: adiciona o exercício 11

---
+++ listas/lista_01/ex_11.py
@@ -0,0 +1,12 @@
+# Estrutura de Controle Sequência - exercício 11
+# Entrada de Dados
+n1 = float(input("Digite o valor de 'n1': "))
+n2 = float(input("Digite o valor de 'n2': "))
+n3 = float(input("Digite o valor de 'n3': "))
+n4 = float(input("Digite o valor de 'n4': "))
+
+# Processamento de Dados
+xbarra = (n1+n2+n3+n4) / 4
+
+# Saída de Dados
+print(f"Resposta 11: a média aritmética é [{xbarra}]")
