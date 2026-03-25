1f69df2b4f3e3bb37638feb82fb4bd64cef2fa2b
André Falcão <datafxlcon@gmail.com>
2026-03-25 14:52:17 -0300

    feat: adiciona o exercício 08

---
+++ listas/lista_01/ex_08.py
@@ -0,0 +1,11 @@
+# Estrutura de Controle Sequência - exercício 08
+# Entrada de Dados
+a = float(input("Digite o valor de 'a': "))
+b = float(input("Digite o valor de 'b': "))
+
+# Saída de Dados
+if a != 0:
+    x = b / a
+    print(f"Resposta 8: {x}")
+else:
+    print("Resposta 8: Equação inválida, dado que a = 0.")
