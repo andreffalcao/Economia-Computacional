8c12d0ca2e4ae3b06037fc8e7ae4bb16e6df0f95
André Falcão <datafxlcon@gmail.com>
2026-03-25 14:38:47 -0300

    feat: adiciona exercício 03

---
+++ listas/lista_01/ex_03.py
@@ -0,0 +1,10 @@
+# Estrutura de Controle Sequência - exercício 03
+# Entrada de Dados
+numero_3 = float(input("Qual o primeiro número: "))
+numero_4 = float(input("Qual o segundo número: "))
+
+# Processamento de Dados
+soma2 = numero_3 + numero_4
+
+# Saída de Dados
+print(f"Resposta 3: {soma2}")
