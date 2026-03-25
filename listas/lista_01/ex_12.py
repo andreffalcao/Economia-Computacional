1e81b05a5b7c4fa0ee13d353b5319af09ba069de
André Falcão <datafxlcon@gmail.com>
2026-03-25 15:10:21 -0300

    feat: adiciona o exercício 12

---
+++ listas/lista_01/ex_12.py
@@ -0,0 +1,10 @@
+# Estrutura de Controle Sequência - exercício 12
+# Entrada de Dados
+r = float(input("Digite o valor do raio: "))
+
+# Processamento de Dados
+areac = 3.14159 * r**2
+circ = 2 * 3.14159 * r
+
+# Saída de Dados
+print(f"Resposta 12: A área do círculo é [{areac:.2f}]; a circunferência é [{circ:.2f}]")
