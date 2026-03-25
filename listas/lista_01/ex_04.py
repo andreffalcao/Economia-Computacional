afdb01045b6cd2f49ab04cb851f83d9c681a080c
André Falcão <datafxlcon@gmail.com>
2026-03-25 14:43:31 -0300

    feat: adiciona o exercício 04

---
+++ listas/lista_01/ex_04.py
@@ -0,0 +1,9 @@
+# Estrutura de Controle Sequência - exercício 04
+# Entrada de Dados
+user = input("De que forma você gostaria de ser chamado? ")
+
+# Processamento de Dados
+huser = (f"Oi, {user} como você está? ")
+
+# Saída de Dados
+print(f"Resposta 4: {huser}")
