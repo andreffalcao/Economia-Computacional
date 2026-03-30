e5ef65c1bce0129aaa67b2b20710ee6b00bd7c8a
André Pinheiro Falcão <datafxlcon@gmail.com>
2026-03-30 14:32:11 -0300

    feat: adiciona o exercício 19

---
+++ listas/lista_01/seleção/ex_19.py
@@ -0,0 +1,9 @@
+# Estrutura de Controle Seleção - exercício 03
+
+# Entrada de Dados
+num = int(input("\nDigite um número: "))
+
+if num % 3 == 0:
+    print(f"Este número é múltiplo de 3.")
+else:
+    print(f"Este número não é múltiplo de 3.")
