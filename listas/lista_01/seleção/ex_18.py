606ce795f14df8fbc16bea2f839976713fa8beaf
André Pinheiro Falcão <datafxlcon@gmail.com>
2026-03-30 14:23:39 -0300

    feat: adiciona o exercício 18
    Added condition to check if the number is odd.

---
+++ listas/lista_01/seleção/ex_18.py
@@ -0,0 +1,12 @@
+# Estrutura de Controle Seleção - exercício 02
+
+# Entrada de Dados
+num = int(input("\nDigite um número: "))
+
+# Processamento e Saída de Dados
+if num % 2 == 0:
+    print(f"O número é par.")
+elif num % 2 != 0:
+    print(f"O número é impar.")
+else:
+    print(f"Este número não é válido.")
