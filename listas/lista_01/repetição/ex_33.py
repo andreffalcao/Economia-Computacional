256c85853532d59063f98aeb5ec337120f382cfe
André Pinheiro Falcão <datafxlcon@gmail.com>
2026-03-31 18:15:27 -0300

    feat: adiciona o exercício 33

---
+++ listas/lista_01/repetição/ex_33.py
@@ -0,0 +1,13 @@
+# Estrutura de Controle Repetição - exercício 08
+
+# Alocação de Memória
+soma = 0
+
+# Processamento de Dados
+for i in range(1,101):
+    soma += i
+
+# Saída de Dados
+print("–" * 40)
+print(f"A soma dos números de 1 até 100 é [{soma}]")
+print("–" * 40)
