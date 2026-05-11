ec13a3591cb612d44534199d3e67e1a9f3ecb75e
André Pinheiro Falcão <datafxlcon@gmail.com>
2026-05-11 19:12:16 -0300

    added header

--- calculadora/operacoes.py
+++ calculadora/operacoes.py
@@ -1,3 +1,13 @@
+"""
+Projeto: Calculadora Modular com suporte a Números Complexos
+Autor: André Pinheiro Falcão
+Disciplina: Economia Computacional
+Descrição: Programa de linha de comando que implementa uma calculadora modular com suporte às quatro operações fundamentais e a números complexos.
+Versão: 1.0.0
+
+Módulo responsável pelas operações matemáticas da calculadora.
+"""
+
 def somar(a, b):
     return a + b
 
